69c3dd3a0de3140d5f865779956163638c45ca37
khaled <khaled_eid77@outlook.com>
2022-12-03 15:13:04 +0200

    The Dashboard Bage first Commit

---
+++ users/admin_home.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="../css/admin_home.css">
+    <script type="text/javascript" src="../js/admin_home.js"></script>
+
+</head>
+
+<body>
+    <div class="wrapper">
+        <!-- Sidebar  -->
+        <section class="nav-section">
+            <h1>Admin</h1>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light nv">
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#dashboard">Dashboard</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#subject">Subject</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">professor</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">transcript</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </section>
+
+        <!-- Page Content  -->
+        <div id="dashboard">
+            <div id="content">
+                <div class="head">
+                    <h1>Students List</h1>
+                </div>
+                <div class="table">
+                    <table class="fixed_headers">
+                        <thead>
+                            <tr>
+                                <th>First name</th>
+                                <th>Last name</th>
+                                <th>Id</th>
+                                <th>National Id</th>
+                                <th>Level</th>
+                                <th>Email</th>
+                                <th>Phone Number</th>
+                                <th>age</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
