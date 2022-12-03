e778490d9c2bcf074dc27b89e9f19c7cce091278
khaled <khaled_eid77@outlook.com>
2022-12-03 16:01:44 +0200

    BOOOOM

--- users/admin_home.php
+++ users/admin_home.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
     <link rel="stylesheet" href="../css/admin_home.css">
-    <script type="text/javascript" src="../js/admin_home.js"></script>
 
 </head>
 
@@ -20,10 +19,10 @@
                 <div class="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul class="navbar-nav">
                         <li class="nav-item active">
-                            <a class="nav-link" href="#dashboard">Dashboard</span></a>
+                            <a class="nav-link" href="#">Dashboard</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Subject</a>
+                            <a class="nav-link" href="subject.php">Subject</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">professor</a>
---
+++ users/subject.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>subject</title>
+    <link rel="stylesheet" href="../css/subject.css">
+
+</head>
+
+<body>
+    <div class="wrapper">
+        <!-- Sidebar  -->
+        <section class="nav-section">
+            <h1>Admin</h1>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light nv">
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="admin_home.php">Dashboard</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Subject</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">professor</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">transcript</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </section>
+
+        <!-- Page Content  -->
+        <div id="dashboard">
+            <div id="content">
+                <div class="header">
+                    <div>
+                        <h1>Subjects</h1>
+                    </div>
+                    <div>
+                        <img src="../images/plus-square.svg" alt="">
+                    </div>
+                </div>
+                <div class="table">
+                    <table class="fixed_headers">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Code</th>
+                                <th>Semester</th>
+                                <th>Level</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</body>
+
+</html>
